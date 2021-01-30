f5b2bf9b3b26353d1f7659e4c25de029e2e1ffb9
SamuelSTPL <samuelstpierrelariviere@gmail.com>
2021-01-30 16:18:58 -0500

    Connected to MongoDB using Mongoose

--- app.js
+++ app.js
@@ -1,38 +1,67 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+require('dotenv').config();
+const db = process.env.DB_URI;
 
-//Create app
+//Create app//
 const app = express();
 
-//Register View engine
+//MongoDB//
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => {
+        //Listen only when connected to DB
+        app.listen(3000);
+        console.log('Connected to DB')})
+    .catch((err) => console.log(err))
+
+//Register View engine//
 app.set('view engine', 'ejs');
 
-//Listen 
-app.listen(3000);
-
-//Morgan
-app.use(morgan('dev'))
+//Middleware//
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'));
 
 
 app.get('/', (req, res) => {
-const blogs = [
-    {title: 'first', snippet: "[nodemon] restarting due to changes..[nodemon] starting `node app.js`"},
-    {title: 'second', snippet: "[nodemon] restarting due to changes..[nodemon] starting `node app.js`"},
-    {title: 'third', snippet: "[nodemon] restarting due to changes..[nodemon] starting `node app.js`"},
-]
-
-    res.render('index', {pageTitle: 'Home', blogs});
+res.redirect('/blogs');
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {pageTitle: 'About'})
 })
 
+
+///Blog routes///
+
+//GET
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1})
+    .then((result) => {
+        res.render('index', {pageTitle: 'All Blogs', blogs: result})
+    })
+    .catch((err) => console.log(err))
+})
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', {pageTitle: 'Create'})
 })
 
-//404 page
+//POST
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+
+    blog.save()
+    .then((result) => {
+        res.redirect('/blogs')
+    })
+    .catch((err) => console.log(err))
+})
+
+
+//404 page//
 app.use((req, res) => {
     res.status(404).render('404', {pageTitle: '404'})
 })
